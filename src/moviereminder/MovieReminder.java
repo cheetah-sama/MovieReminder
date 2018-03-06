71427119e22415e1c36aa11ef68ec65ed57965f4
RedDragon <RedDragon@RedDragon>
2018-03-07 01:58:01 +0300

    added better output!

--- src/moviereminder/MovieReminder.java
+++ src/moviereminder/MovieReminder.java
@@ -10,7 +10,7 @@ public class MovieReminder {
     
     
     public static void main(String[] args) {
-        System.out.println("anything");
+        System.out.println("anything wow");
     }
     
 }
