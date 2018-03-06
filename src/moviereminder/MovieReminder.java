92e76d7884dd3817cfa097f07e6c7a54479b0f2a
fahad <fahad@DESKTOP-UN6C1OU>
2018-03-07 01:45:41 +0300

    added cool output

---
+++ src/moviereminder/MovieReminder.java
@@ -0,0 +1,16 @@
+
+package moviereminder;
+
+/**
+ *
+ * @author fahad
+ */
+public class MovieReminder {
+
+    
+    
+    public static void main(String[] args) {
+        System.out.println("anything");
+    }
+    
+}
